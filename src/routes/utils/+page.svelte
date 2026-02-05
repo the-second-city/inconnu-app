d56a4181c07ffd145e6eef0a58c785c8f96b344d
tiltowait <208040+tiltowait@users.noreply.github.com>
2026-02-04 22:31:22 -0800

    Tweaks

--- src/routes/utils/+page.svelte
+++ src/routes/utils/+page.svelte
@@ -19,13 +19,20 @@
 
 <h1 class="h1">Utilities</h1>
 
+<p class="px-2">
+	A small but growing collection of random play aids for <em>Vampore: The Masquerade</em>.
+</p>
+
 <a class="block transition-transform hover:scale-101" href="/utils/beasty" title="Beasty">
 	<Card>
 		{@render heading('Beasty')}
 		<p>
-			For fans of Discord play-by-post (or anywhere that accepts Markdown), give your writing some
-			flair by formatting your character's internal monologue as "Beast speak," alternating between
-			bold and italics.
+			For fans of Discord play-by-post (or anywhere that accepts <a
+				class="anchor"
+				href="https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline"
+				title="Markdown overview">Markdown</a
+			>), give your writing some flair by formatting your character's internal monologue as "Beast
+			speak," alternating between bold and italics.
 		</p>
 	</Card>
 </a>
